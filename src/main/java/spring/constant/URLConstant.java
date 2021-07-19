fa3e3ee33d45d9b9a16122021dfb62defd9e4817
Tronghien <andrew.hien98@gmail.com>
2021-07-19 22:48:30 +0700

    aa

--- src/main/java/spring/constant/URLConstant.java
+++ src/main/java/spring/constant/URLConstant.java
@@ -14,14 +14,22 @@ public class URLConstant {
 	
 	public static final String URL_ADMIN = "admin";
 	public static final String URL_ADMIN_INDEX = "trang-chu.html";
+	public static final String URL_ADMIN_CAT_ADD = "danh-muc/them-danh-muc.html";
+	public static final String URL_ADMIN_CAT_EDIT = "danh-muc/sua-danh-muc-{id}.html";
 	public static final String URL_ADMIN_CAT_INDEX = "danh-muc.html";
-
+	public static final String URL_ADMIN_PRODUCT_INDEX = "san-pham.html";
 
 	
 	public static final String ERROR = "error";
 	public static final String ERROR_404 = "404";
 	public static final String ERROR_403 = "403";
 	public static final String ERROR_400 = "400";
+
+	
+
+	
+
+	
 	
 
 	
